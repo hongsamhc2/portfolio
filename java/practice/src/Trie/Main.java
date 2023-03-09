337d15afd77fbc4f70eae5cc3e5db0e09dc10d5d
hongsamhc2 <hongsamgcp@gmail.com>
2023-03-09 11:03:12 +0900

    [MOD] JAVA Trie

--- java/practice/src/Trie/Main.java
+++ java/practice/src/Trie/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Trie trie = new Trie();
         trie.insert("text");
+        System.out.println(trie.search("text"));
         System.out.println(trie.search("tex"));
     }
 }
