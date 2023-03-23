6afd79523717b43ea8bd7539ae038d0f8f64a77c
hongsamhc2 <hongsamgcp@gmail.com>
2023-03-23 18:15:58 +0900

    ADD:JAVA ACHOCORASICK

---
+++ java/practice/src/Achocorasick/App.java
@@ -0,0 +1,8 @@
+package Achocorasick;
+
+public class App {
+    public static void main(String[] args) {
+        AhocorasickTrie trie = new AhocorasickTrie();
+        trie.add("sdjnsdjn");
+    }
+}
